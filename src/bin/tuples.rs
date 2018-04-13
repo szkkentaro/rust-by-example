b1db61c4e93aa68aea9025de7648ec0eb8e05aa9
Kentaro SUZUKI <kentarock@gmail.com>
2018-04-14 06:25:24 +0900

    refactor: Add transpose function for tuples

--- src/bin/tuples.rs
+++ src/bin/tuples.rs
@@ -5,12 +5,16 @@ fn reverse(pair: (i32, bool)) -> (bool, i32) {
     (boolean, integer)
 }
 
+fn transpose(matrix: Matrix) -> Matrix {
+    Matrix(matrix.0, matrix.2, matrix.1, matrix.3)
+}
+
 #[derive(Debug)]
 struct Matrix(f32, f32, f32, f32);
 
 impl fmt::Display for Matrix {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({} {})\n({} {})", self.0, self.1, self.2, self.3)
+        write!(f, "( {} {} )\n( {} {} )", self.0, self.1, self.2, self.3)
     }
 }
 
@@ -52,5 +56,6 @@ fn main() {
     let matrix = Matrix(1.2, 2.3, 3.4, 4.5);
     println!("{:?}", matrix);
 
-    println!("{}", matrix);
+    println!("Matrix:\n{}", matrix);
+    println!("Transpose:\n{}", transpose(matrix));
 }
