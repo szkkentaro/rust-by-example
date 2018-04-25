b421246b90b43c4cdc00928fea295d479721b780
Kentaro SUZUKI <kentarock@gmail.com>
2018-04-26 08:39:19 +0900

    refactor: Add literal samples

--- src/bin/literals.rs
+++ src/bin/literals.rs
@@ -17,4 +17,20 @@ fn main() {
     println!("0x80 >> 2 is {}", 0x80u32 >> 2);
 
     println!("One million is written as {}", 1_000_000u32);
+
+    // Fixed literals, their types are known at initalization
+    let x = 1u8;
+    let y = 2u32;
+    let z = 3f32;
+
+    // Unsuffixed literals, their types depend on how they are used
+    let i = 1;
+    let f = 1.0;
+
+    // `size_of_val` returns the size of variable in bytes
+    println!("size of `x` in bytes: {}", std::mem::size_of_val(&x));
+    println!("size of `y` in bytes: {}", std::mem::size_of_val(&y));
+    println!("size of `z` in bytes: {}", std::mem::size_of_val(&z));
+    println!("size of `i` in bytes: {}", std::mem::size_of_val(&i));
+    println!("size of `f` in bytes: {}", std::mem::size_of_val(&f));
 }
