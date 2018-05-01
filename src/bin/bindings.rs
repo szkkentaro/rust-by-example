c8119dc37b0ee5185918d38d336169842b8e6627
Kentaro SUZUKI <kentarock@gmail.com>
2018-05-01 09:05:16 +0900

    feat: Add bindings

---
+++ src/bin/bindings.rs
@@ -0,0 +1,13 @@
+fn age() -> u32 {
+    15
+}
+
+fn main() {
+    println!("Tell me type of person you are");
+    match age() {
+        0 => println!("Iam not born yet I guess"),
+        n @ 1...12 => println!("I'm a child of age {}", n),
+        n @ 13...19 => println!("I'm a teen of age {}", n),
+        n => println!("I'm a old person of age {}", n),
+    }
+}
