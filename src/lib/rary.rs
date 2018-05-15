8c48d06885774a5d6c062cae4b52af949083acdf
Kentaro SUZUKI <kentarock@gmail.com>
2018-05-16 08:52:39 +0900

    feat: Add lib

---
+++ src/lib/rary.rs
@@ -0,0 +1,10 @@
+pub fn public_function() {
+    println!("called rary's function()");
+}
+fn private_function() {
+    println!("called rary's private_function()");
+}
+pub fn indirect_access() {
+    print!("called rary's indirect_access(), that\n> ");
+    private_function();
+}
