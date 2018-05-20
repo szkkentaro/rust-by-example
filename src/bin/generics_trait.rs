4b624ab674512f697f35320cbe8fb91bb707932b
Kentaro SUZUKI <kentarock@gmail.com>
2018-05-21 08:52:19 +0900

    feat: Add generics trait

---
+++ src/bin/generics_trait.rs
@@ -0,0 +1,22 @@
+struct Empty;
+struct Null;
+
+trait DoubleDrop<T> {
+    fn double_drop(self, _: T);
+}
+// T is generic param, U is caller
+impl<T, U> DoubleDrop<T> for U {
+    // This method takes owenership of both passed arguments,
+    fn double_drop(self, _: T) {}
+}
+
+fn main() {
+    let empty = Empty;
+    let null = Null;
+
+    // Deallocate `empty` and `null`
+    empty.double_drop(null);
+
+    empty;
+    null;
+}
