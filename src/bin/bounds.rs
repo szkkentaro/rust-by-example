f31758cd9132cc1db389799d6d19d7a635fd9532
Kentaro SUZUKI <kentarock@gmail.com>
2018-05-22 09:21:43 +0900

    feat: Add bounds

---
+++ src/bin/bounds.rs
@@ -0,0 +1,51 @@
+// Define a function `printer` that takes a generic type `T` which must implement trait `Display`
+// fn printer<T: Display>(t: T) {
+//     println!("{}", t);
+// }
+
+// A trait which implements the print maker: `{:?}`
+use std::fmt::Debug;
+
+trait HasArea {
+    fn area(&self) -> f64;
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    length: f64,
+    height: f64,
+}
+
+impl HasArea for Rectangle {
+    fn area(&self) -> f64 {
+        self.length * self.height
+    }
+}
+
+#[allow(dead_code)]
+struct Triangle {
+    length: f64,
+    height: f64,
+}
+
+fn print_debug<T: Debug>(t: &T) {
+    println!("{:?}", t);
+}
+
+fn area<T: HasArea>(t: &T) -> f64 {
+    t.area()
+}
+
+fn main() {
+    let rectangle = Rectangle {
+        length: 3.0,
+        height: 4.0,
+    };
+    let _triangle = Triangle {
+        length: 3.0,
+        height: 4.0,
+    };
+
+    print_debug(&rectangle);
+    println!("Area {}:", area(&rectangle));
+}
