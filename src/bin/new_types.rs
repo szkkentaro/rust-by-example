5b3e22a32beb2d84b2750a28a24e8553abc0d81a
Kentaro SUZUKI <kentarock@gmail.com>
2018-05-26 16:01:50 +0900

    feat: Add new types

---
+++ src/bin/new_types.rs
@@ -0,0 +1,24 @@
+struct Years(i64);
+struct Days(i64);
+
+impl Years {
+    fn to_days(&self) -> Days {
+        Days(self.0 * 365)
+    }
+}
+impl Days {
+    fn to_years(&self) -> Years {
+        Years(self.0 / 365)
+    }
+}
+
+fn old_enough(age: &Years) -> bool {
+    age.0 >= 18
+}
+
+fn main() {
+    let age = Years(18);
+    let age_days = age.to_days();
+    println!("Old enough {}", old_enough(&age));
+    println!("Old enough {}", old_enough(&age_days.to_years()));
+}
