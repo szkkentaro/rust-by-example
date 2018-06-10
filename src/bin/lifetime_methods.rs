c58b1527463bb1c2261fde7ddce17864d8c7d578
Kentaro SUZUKI <kentarock@gmail.com>
2018-06-10 10:51:36 +0900

    feat: Add lifetime bounds

---
+++ src/bin/lifetime_methods.rs
@@ -0,0 +1,18 @@
+// Methods are annotated similarly to functions:
+
+struct Owner(i32);
+
+impl Owner {
+    fn add_one<'a>(&'a mut self) {
+        self.0 += 1;
+    }
+    fn print<'a>(&'a self) {
+        println!("`print`: {}", self.0)
+    }
+}
+
+fn main() {
+    let mut owner = Owner(20);
+    owner.add_one();
+    owner.print();
+}
