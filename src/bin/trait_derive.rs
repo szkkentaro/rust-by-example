646d35ad25be244e9ae0bd14f113ac5f29db6290
Kentaro SUZUKI <kentarock@gmail.com>
2018-06-17 16:21:14 +0900

    feat: Add trait derive

---
+++ src/bin/trait_derive.rs
@@ -0,0 +1,34 @@
+#[derive(PartialEq, PartialOrd)]
+struct Centimeters(f64);
+
+#[derive(Debug)]
+struct Inches(i32);
+
+impl Inches {
+    fn to_centimeters(&self) -> Centimeters {
+        let &Inches(inches) = self;
+        Centimeters(inches as f64 * 2.54)
+    }
+}
+
+struct Seconds(i32);
+
+fn main() {
+    let _one_second = Seconds(1);
+
+    // Seconds type can't be printed. It does'nt implement the `Debug`
+    // println!("{:?}", _one_second);
+
+    let foot = Inches(12);
+    println!("{:?}", foot);
+
+    let meter = Centimeters(100.0);
+
+    let cmp = if foot.to_centimeters() < meter {
+        "smaller"
+    } else {
+        "bigger"
+    };
+
+    println!("One foot is {} than one meter.", cmp);
+}
