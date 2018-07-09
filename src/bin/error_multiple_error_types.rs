ecef3a4170af4b6fadb1845de24cb1194c01d469
Kentaro SUZUKI <kentarock@gmail.com>
2018-07-10 08:28:51 +0900

    feat: Add error multiple error types

---
+++ src/bin/error_multiple_error_types.rs
@@ -0,0 +1,16 @@
+// Multiple error types
+
+fn double_first(vec: Vec<&str>) -> i32 {
+    let first = vec.first().unwrap(); // Generate error 1
+    2 * first.parse::<i32>().unwrap() // Generate error 2
+}
+
+fn main() {
+    let numbers = vec!["42", "93", "18"];
+    let empty = vec![];
+    let strings = vec!["tofu", "93", "18"];
+
+    println!("The first doubled is {}", double_first(numbers));
+    println!("The first doubled is {}", double_first(empty));
+    println!("The first doubled is {}", double_first(strings));
+}
