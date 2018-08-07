41e795ea6a349bdceb676e3d0910f9fd19374fc3
Kentaro SUZUKI <kentarock@gmail.com>
2018-08-08 08:53:17 +0900

    feat: Add channels

---
+++ src/bin/channels.rs
@@ -0,0 +1,35 @@
+// Channels
+
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
+use std::thread;
+
+static NTHREADS: i32 = 3;
+
+fn main() {
+    // Channels have two endpoints
+    let (tx, rx): (Sender<i32>, Receiver<i32>) = mpsc::channel();
+
+    for id in 0..NTHREADS {
+        // The sender endpoint can be copied
+        let thread_tx = tx.clone();
+
+        // Each thread will send its id via the channel
+        thread::spawn(move || {
+            thread_tx.send(id).unwrap();
+            // sending is a non-blocking operationm, the thread will continue
+            // immediately after sending its message
+            println!("thread {} finished", id);
+        });
+    }
+
+    let mut ids = Vec::with_capacity(NTHREADS as usize);
+    for _ in 0..NTHREADS {
+        // The `recv` method picks a message from the channel
+        // `recv` will block the current thread if there are no messages available
+        ids.push(rx.recv());
+    }
+
+    // SHow the order in which the messages were sent
+    println!("{:?}", ids);
+}
