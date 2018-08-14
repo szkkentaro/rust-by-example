39cb66a347d37937848a1c437dd869c41175646f
Kentaro SUZUKI <kentarock@gmail.com>
2018-08-15 08:58:42 +0900

    feat: Add standard library

---
+++ src/bin/wait.rs
@@ -0,0 +1,9 @@
+// Wait
+
+use std::process::Command;
+
+fn main() {
+    let mut child = Command::new("sleep").arg("5").spawn().unwrap();
+    let result = child.wait().unwrap();
+    println!("reached end of main. result code :{}", result);
+}
