2c201bd6a4fe875e036a9aca6dc990ffed55240e
Kentaro SUZUKI <kentarock@gmail.com>
2018-08-17 08:55:36 +0900

    feat: Add standard lib

---
+++ src/bin/standard_library.rs
@@ -0,0 +1,10 @@
+// Program arguments
+// Standard Library
+
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!("My path is {}", args[0]);
+    println!("I got {:?} arguments: {:?}", args.len() - 1, &args[1..]);
+}
