878cdfdfaa39ac08fc5669fe46bf1de39def7a9d
Kentaro SUZUKI <kentarock@gmail.com>
2018-08-21 09:35:52 +0900

    feat: Add ignoring tests

---
+++ src/bin/ignoring_tests.rs
@@ -0,0 +1,31 @@
+// Ignoring tests
+
+// Run ignoring test only
+// cargo test --bin ignoring_tests -- --ignored
+
+// Run test
+// cargo test --bin ignoring_tests
+
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_add() {
+        assert_eq!(add(2, 2), 4);
+    }
+    #[test]
+    fn test_add_hundred() {
+        assert_eq!(add(100, 2), 102);
+        assert_eq!(add(2, 100), 102);
+    }
+
+    #[test]
+    #[ignore]
+    fn ignored_test() {
+        assert_eq!(add(0, 0), 0);
+    }
+}
